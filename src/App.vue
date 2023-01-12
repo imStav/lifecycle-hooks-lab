e956c446fbd99ff95c4830eb7433713d0630b420
imStav <saprocontact@gmail.com>
2023-01-12 16:10:15 +0100

    Vue 3 Lifecycle Hooks Lab!
    Initializing project.
    
    A sandbox to test and visualize Vue 3 Lifecycle Hooks, using the Composition API.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav a {
+  margin-inline: 1rem;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  ref,
+} from "vue";
+
+const el = ref();
+let count = ref(0);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+
+onMounted(() => {
+  console.log("onMounted", el.value);
+});
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+});
+
+onUpdated(() => {
+  console.log("onUpdated");
+});
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount");
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+
+  <div ref="el" @click="count++" class="counter">
+    {{ count }}
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  margin-block: 2rem;
+  padding: 1rem 1.5rem;
+  border: 1px solid gray;
+  border-radius: 8px;
+  width: fit-content;
+}
+</style>
