78f63437d5b77daddd816e2eaf5bf0c239af9ba4
imStav <saprocontact@gmail.com>
2023-01-16 10:03:31 +0100

    Changed **views** names
    Changed AboutView's name to be HomeView, and HomeView to be AboutView.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,58 +1,9 @@
 <script setup>
-import {
-  onBeforeMount,
-  onMounted,
-  onBeforeUpdate,
-  onUpdated,
-  onBeforeUnmount,
-  onUnmounted,
-  ref,
-} from "vue";
-
-const el = ref();
-let count = ref(0);
-
-onBeforeMount(() => {
-  console.log("onBeforeMount");
-});
-
-onMounted(() => {
-  console.log("onMounted", el.value);
-});
-
-onBeforeUpdate(() => {
-  console.log("onBeforeUpdate");
-});
-
-onUpdated(() => {
-  console.log("onUpdated");
-});
-
-onBeforeUnmount(() => {
-  console.log("onBeforeUnmount");
-});
-
-onUnmounted(() => {
-  console.log("onUnmounted");
-});
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-
-  <div ref="el" @click="count++" class="counter">
-    {{ count }}
-  </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
-
-<style scoped>
-.counter {
-  margin-block: 2rem;
-  padding: 1rem 1.5rem;
-  border: 1px solid gray;
-  border-radius: 8px;
-  width: fit-content;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  ref,
+} from "vue";
+
+const pageTitle = ref("Lifecycle Hooks Lab");
+const el = ref();
+let count = ref(0);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+
+onMounted(() => {
+  console.log("onMounted", el.value);
+});
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+});
+
+onUpdated(() => {
+  console.log("onUpdated", el.value);
+});
+
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount");
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted", el.value);
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <h1>{{ pageTitle }}</h1>
+  </div>
+
+  <div ref="el" @click="count++" class="counter">
+    {{ count }}
+  </div>
 </template>
+
+<style scoped>
+.counter {
+  margin-block: 2rem;
+  padding: 1rem 1.5rem;
+  border: 1px solid gray;
+  border-radius: 8px;
+  width: fit-content;
+}
+</style>
